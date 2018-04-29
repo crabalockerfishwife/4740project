77ecd3b01282b3d8a3e93a51ec71aed1da016948
Fish Milnikiewicz <sam464@cornell.edu>
2018-04-28 23:04:36 -0400

    fixed genre

--- cleaning_script.r
+++ cleaning_script.r
@@ -5,11 +5,11 @@ library(lubridate)
 View(movies_metadata) # View original dataset
 
 # Select the fields we're interested in
-cleaned_movies_metadata <- movies_metadata[, c("revenue", "budget", "genres", "original_language", "production_companies",
+cleaned_movies_metadata <- movies_metadata[, c("revenue", "budget", "genres", "original_language", 
                                                "release_date", "runtime") ]
 
 # Remove rows containing zeros
-cleaned_movies_metadata <- cleaned_movies_metadata[!apply(cleaned_movies_metadata[,1:7] == 0, 1, FUN = any, na.rm = TRUE),]
+cleaned_movies_metadata <- cleaned_movies_metadata[!apply(cleaned_movies_metadata[,1:6] == 0, 1, FUN = any, na.rm = TRUE),]
 
 # Convert original language to a factor field
 cleaned_movies_metadata$original_language <- as.factor(cleaned_movies_metadata$original_language)
@@ -22,8 +22,8 @@ cleaned_movies_metadata <- cleaned_movies_metadata %>%
 cleaned_movies_metadata$month <- as.factor(cleaned_movies_metadata$month)
 
 # Get rid of unneccessary release_date field now
-cleaned_movies_metadata <- cleaned_movies_metadata[, c("revenue", "budget", "genres", "original_language", "production_companies",
-                                               "year", "month", "runtime") ]
+cleaned_movies_metadata <- cleaned_movies_metadata[, c("revenue", "budget", "genres", "original_language", 
+                                                       "year", "month", "runtime") ]
 
 # Convert original language to an integer field
 cleaned_movies_metadata$runtime <- as.integer(cleaned_movies_metadata$runtime)
@@ -33,8 +33,25 @@ cleaned_movies_metadata <- cleaned_movies_metadata[which(cleaned_movies_metadata
 
 # Get rid of rows containing "[]" for genres and production_companies
 cleaned_movies_metadata <- cleaned_movies_metadata[which(cleaned_movies_metadata$genres != "[]"),]
-cleaned_movies_metadata <- cleaned_movies_metadata[which(cleaned_movies_metadata$production_companies != "[]"),]
 
+# Need this library to deal with the formatting of the genres field
+library(jsonlite)
+
+cleaned_movies_metadata$genres <- gsub("'", '"', cleaned_movies_metadata$genres)
+
+cleaned_movies_metadata$genre <- "None"
+
+# Iterate through each row/movie, select its first genre and make that the value of the new genre field
+for ( i in 1:nrow(cleaned_movies_metadata) ){
+  json <- toString(cleaned_movies_metadata$genres[i])
+  mydf <- fromJSON(json)
+  genre <- mydf[1,2]
+  cleaned_movies_metadata$genre[i] = genre
+}
+
+# Get rid of unneccessary fields genres
+cleaned_movies_metadata <- cleaned_movies_metadata[, c("revenue", "budget", "original_language", 
+                                                       "year", "month", "runtime", "genre") ]
 
 # Use these to view/see and summary of the data
 View(cleaned_movies_metadata)
